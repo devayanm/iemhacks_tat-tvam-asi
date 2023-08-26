ab9e5ff16f80c7444652db5850e6ed237574d09f
Devayan <devayan9689@gmail.com>
2023-08-26 23:33:14 +0530

    Added Joinus page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import { Navbar, Footer } from './components';
 import { Routes, Route } from "react-router-dom";
-import { HomePage } from './Pages';
+import { HomePage, JoinUsPage } from './Pages';
 
 function App() {
   return (
@@ -10,6 +10,7 @@ function App() {
       <Routes>
 
         <Route exact path="/" element={<HomePage />} />
+        <Route path='join-us' element={<JoinUsPage />} />
       </Routes>
       <Footer />
     </div>
