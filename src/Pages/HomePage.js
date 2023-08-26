80faeda6fe256cdae63fa0f382df1a127428db4e
Devayan <devayan9689@gmail.com>
2023-08-26 19:32:50 +0530

    Fixed issues with Header and Navbar

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,13 +1,10 @@
 import React from 'react'
-import { Features, BlogCarousel, VideosCarousel, Header } from '../components'
+import { Header } from '../components'
 
 const HomePage = () => {
     return (
         <div>
             <Header />
-            <Features />
-            <BlogCarousel />
-            <VideosCarousel />
         </div>
     )
 }
--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,6 @@
 export {default as Navbar} from './Navbar/Navbar'
-export {default as Footer} from './Footer/Footer'
-// export {default as Header} from './Header/Header'
+// export {default as Footer} from './Footer/Footer'
+export {default as Header} from './Header/Header'
 // export {default as Features} from './Features/Features'
 // export {default as ArticleCarousel} from './Resource/Article Carousel/ArticleCarousel'
 // export {default as BlogCarousel} from './Resource/Blog Carousel/BlogCarousel'
