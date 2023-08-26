2bdb9daccf7efd84bb6077374d7b645a65be582a
Devayan <devayan9689@gmail.com>
2023-08-27 01:32:38 +0530

    Added Blog, Video, Article Carousel

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,11 +1,14 @@
 import React from 'react'
-import { Header, Features } from '../components'
+import { Header, Features, BlogCarousel, VideosCarousel, ArticleCarousel } from '../components'
 
 const HomePage = () => {
     return (
         <div>
             <Header />
             <Features />
+            <BlogCarousel />
+            <VideosCarousel />
+            <ArticleCarousel />
         </div>
     )
 }
